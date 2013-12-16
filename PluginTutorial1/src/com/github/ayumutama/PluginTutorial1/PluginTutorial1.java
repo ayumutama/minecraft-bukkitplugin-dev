8a5c1d16f50d4d9a3f28b22152be93dda42022a6
ayumutama <ayumutama@hotmail.co.jp>
2013-12-16 18:58:00 +0900

    コンパイル済みjar削除
    よく考えたら自分でコンパイルしてもらうためにソースあるんだよね・・・

--- PluginTutorial1/src/com/github/ayumutama/PluginTutorial1/PluginTutorial1.java
+++ PluginTutorial1/src/com/github/ayumutama/PluginTutorial1/PluginTutorial1.java
@@ -1,13 +1,18 @@
 package com.github.ayumutama.PluginTutorial1;
 
+//APIからインポート
 import org.bukkit.plugin.java.JavaPlugin;
 
+//クラス作成
 public class PluginTutorial1 extends JavaPlugin {
 
+	//プラグインが有効化されたら実行
 	public void onEnable(){
+		//info()内が表示内容
 		getLogger().info("This plugin has been successfully enabled!");
 	}
 
+	//プラグインが無効化されたら実行
 	public void onDisable(){
 		getLogger().info("This plugin has been successfully disabled...bye!");
 	}
