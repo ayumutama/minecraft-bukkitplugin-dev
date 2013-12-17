c5057f952ed95e0038de56ad2f0c21928086db33
ayumutama <ayumutama@hotmail.co.jp>
2013-12-17 09:56:18 +0900

    プラグイン「コマンド実行3」開発終了
    debug完了。bukkitでの正常動作を確認しました

--- PluginTutorial4/src/com/github/ayumutama/PluginTutorial4/PluginTutorial4.java
+++ PluginTutorial4/src/com/github/ayumutama/PluginTutorial4/PluginTutorial4.java
@@ -6,13 +6,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 //クラス作成
 public class PluginTutorial4 extends JavaPlugin {
 
-	private cmdtest command1;
 	@Override
+	//プラグイン有効化時
 	public void onEnable(){
+		//メッセ表示
+		getLogger().info("This plugin has been successfully enabled!");
+		//コマンド受け取り待ち)cmdtest.javaにコマンド定義
+		getCommand("testcmd").setExecutor(new cmdtest(this));
 		
-		command1 = new cmdtest(this);
-		getCommand("cmdtest").setExecutor(command1);
-		
+	}
+	
+
+	//プラグインが無効化されたら実行
+	public void onDisable(){
+		getLogger().info("This plugin has been successfully disabled...bye!");
 	}
 	
 }
--- PluginTutorial4/src/com/github/ayumutama/PluginTutorial4/cmdtest.java
+++ PluginTutorial4/src/com/github/ayumutama/PluginTutorial4/cmdtest.java
@@ -6,14 +6,17 @@ import org.bukkit.command.CommandSender;
 
 public class cmdtest implements CommandExecutor{
 	
+	//pluginに警告が出るが問題なく実行可能
 	private PluginTutorial4 plugin;
 	
+	//この辺の処理は詳しく調べないと忘れた・・・
 	public cmdtest(PluginTutorial4 plugin){
 		
 		this.plugin = plugin;
 		
 	}
 
+	//以下処理はコマンド実行のプログラムと書き方は一緒
 	@Override
 	//command設定メゾット
 		public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
